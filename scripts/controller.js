ca285e4c278972ea4df8ea243b72415bd3ec1933
AlexVilla <alex.villa.v@gmail.com>
2017-05-16 17:20:35 -0400

    userdata

--- scripts/controller.js
+++ scripts/controller.js
@@ -54,7 +54,8 @@ angular.module('metrogas')
 .controller('SideNavCtrl', ['$scope', '$ionicSideMenuDelegate', '$state', 'UserService', function ($scope, $ionicSideMenuDelegate, $state, UserService) {
     
     $scope.user = JSON.parse(window.localStorage.getItem('user'));
-        
+    console.log($scope.user);
+    
     $scope.toggleMenu = function() {
         $ionicSideMenuDelegate.toggleLeft();
     };
