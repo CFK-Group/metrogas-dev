a900d1817ad10523557f25d935e6daa13bd43577
AlexVilla <alex.villa.v@gmail.com>
2017-05-18 18:10:44 -0400

    filter

--- scripts/controller.js
+++ scripts/controller.js
@@ -112,11 +112,11 @@ angular.module('metrogas')
     localStorage.setItem('direcciones', $scope.direcciones);
 
     $scope.allComunas = JSON.parse(localStorage.getItem('comunas'));
-    $scope.allCalles = ventasService.getCalles();
-    $scope.allGrilla = ventasService.getGrillas();
+    $scope.allCalles = JSON.parse(localStorage.getItem('calles'));
+    $scope.allGrilla = JSON.parse(localStorage.getItem('grillas'));
     $scope.allRecorrida = [{"value": 0}, {"value": 1}];
     $scope.allContactada = [{"value": 0}, {"value": 1}];
-    
+
     $scope.filterOptions={
         calle: "",
         comuna: "",
