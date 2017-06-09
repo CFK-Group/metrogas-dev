dd98aaa8973cecd9345b5f3118897ec93ae23d18
Unknown <alex.villa.v@gmail.com>
2017-06-09 18:13:22 -0400

    add equivalent function

--- scripts/controller.js
+++ scripts/controller.js
@@ -157,9 +157,31 @@ angular.module('metrogas')
 
     setInterval(function () {
         console.log($scope.model);
-        console.log($scope.model == $scope.direccion);
+        console.log(isEquivalent($scope.model, $scope.direccion));
     }, 1000);
 
+    function isEquivalent(a, b) {
+        // Create arrays of property names
+        var aProps = Object.getOwnPropertyNames(a);
+        var bProps = Object.getOwnPropertyNames(b);
+        // If number of properties is different,
+        // objects are not equivalent
+        if (aProps.length != bProps.length) {
+            return false;
+        }
+        for (var i = 0; i < aProps.length; i++) {
+            var propName = aProps[i];
+
+            // If values of same property are not equal,
+            // objects are not equivalent
+            if (a[propName] !== b[propName]) {
+                return false;
+            }
+        }
+        // If we made it this far, objects
+        // are considered equivalent
+        return true;
+    }
 
 }])
 ;
