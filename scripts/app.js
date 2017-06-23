41a57a919b11fdacf3b668ec89ef5a43a6c5b12d
Unknown <alex.villa.v@gmail.com>
2017-06-22 23:55:57 -0400

    button and routing

--- scripts/app.js
+++ scripts/app.js
@@ -90,6 +90,14 @@ angular.module('metrogas', ['ionic','ui.router','ngCordova','angular.filter','ng
                     $state.go('app.asignadas');
                 }
             }
+            else if($state.current.name==="app.edit" && $stateParams.from === 'historial')
+            {
+                if($ionicSideMenuDelegate.isOpen()) {
+                    $ionicSideMenuDelegate.toggleLeft(false);
+                }else {
+                    $state.go('app.historial');
+                }
+            }
             else if($state.current.name==="app.accioncomercial" && $stateParams.from === "edit" )
             {
                 if($ionicSideMenuDelegate.isOpen()) {
